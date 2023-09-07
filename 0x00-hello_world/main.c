ad088188a5b0e6841f691e988dfa4387ec659f7b
martinmativo <martinmativo.m@gmail.com>
2023-09-07 11:05:24 -0700

    correction 1

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+    printf("Holberton School\n");
     return (0);
 }
