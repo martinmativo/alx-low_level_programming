2e1639e1a19dd3e083a3eaf6ac66d4098eb357d6
martinmativo <martinmativo.m@gmail.com>
2023-09-07 13:00:18 -0700

    Task for a C program that prints with put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a  C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
