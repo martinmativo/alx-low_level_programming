76a690e6cb96abd773db55c3d9e2702facbf1b9b
martinmativo <martinmativo.m@gmail.com>
2023-10-04 15:11:31 -0700

    fix for 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,9 +12,9 @@
  *
  * Return: Always 0.
  */
-int main (int __attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-        printf("%s\n)",argv[0]);
+	printf("%s\n", argv[0]);
 
-        return (0);
+	return (0);
 }
