849591d0b5ca12dcb7787e847bce28c2b970f35c
martinmativo <martinmativo.m@gmail.com>
2023-10-04 11:39:50 -0700

    added tasks for the 0x0A-argc_argv module

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,21 +15,18 @@
  */
 int main(int argc, char *argv[])
 {	
-	int num1;
-	int num2;
-	int prod;
+	int num1, num2,prod
 
 	if (argc != 3)
 	{
 		printf("error!");
-
 		return (1);
 
 	}
 
-	num1=atoi(argv[1]);
-	num2=atoi(argv[2]);
-	prod= num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 	
 	printf("%d\n",prod);
 
