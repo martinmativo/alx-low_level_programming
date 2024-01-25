c08551873c6a782f9222c992d89cbe10fbf47750
martinmativo <mativomartin.m@gmail.com>
2024-01-25 15:42:43 -0800

    fix task 7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,20 +4,20 @@
  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
  *                            list at a given position.
  * @h: A pointer to the head of the dlistint_t list.
- * @psx: The position to insert the new node.
- * @m: The integer for the new node to contain.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int psx, int m)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
 
-	if (psx == 0)
-		return (add_dnodeint(h, m));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	for (; psx != 1; psx--)
+	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
@@ -25,13 +25,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int psx, int m)
 	}
 
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, m));
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->m = m;
+	new->n = n;
 	new->prev = tmp;
 	new->next = tmp->next;
 	tmp->next->prev = new;
